405ea73d4f1bef7fc022e2fc9d0e682cf0293c68
Aditya Dev <adityadev91@rediffmail.com>
2015-07-31 22:22:51 -0700

    Initial commit to master

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Swades - Indian Student Association at Santa Clara University</title>
+	<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+	<meta content="utf-8" http-equiv="encoding">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="stylesheet" href="css/index.css" type="text/css" media="screen, projection">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/index.js"></script>
+</head>
+<body>
+
+<div class="container">
+	<div class="jumbotron">
+		<h1><img src="images/swadeslogo.png" class="img-circle" alt="Swades Logo" width="20%"/>Welcome to Swades</h1>
+		<p>Swades is the Indian Students Association at Santa Clara University. With an approximate following of 300 students on-campus and over 1000 Aluminis, It happens to be the bigest International students group at SCU.</p>
+
+<p>Swades was formed in 2009, with the aim of uniting all the Indian students at SCU under a common roof. Over the years, the strength has increased manifold and swades today, stands to be among the most active students Organization at SCU.</p> 
+	</div>
+	<div class="row">
+		<div class="col-sm-4" id="listContainer">
+			<h2>The Team</h2>
+			<h3>Swades Current Board Members</h3>
+			<div class="listControl">
+				<a id="expandList">Expand All</a>
+				<a id="collapseList">Collapse All</a>
+			</div>
+			<ul id="expList">
+				<li>
+					Anand Ramchandani - President
+					<ul>
+						<li>
+							
+						</li>
+					</ul>
+				</li>
+				<li>Raghav Jayaraman - Vice President<ul>
+						<li>
+							
+						</li>
+					</ul>
+				</li>
+				<li>Ushita Shah - General Secretary<ul>
+						<li>
+							
+						</li>
+					</ul>
+				</li>
+				<li>Harsha Teja - Web Developer<ul>
+						<li>
+							
+						</li>
+					</ul>
+				</li>
+				<li>Aditya Dev - Web Developer<ul>
+						<li>
+							
+						</li>
+					</ul>
+				</li>
+				<li>Ruchi Khurana - Cultural Secretary<ul>
+						<li>
+							
+						</li>
+					</ul>
+				</li>
+				<li>Kushal Munot - Sports Secretary<ul>
+						<li>
+							
+						</li>
+					</ul>
+				</li>
+				<li>Venu Gopal - Event Coordinator<ul>
+						<li>
+							
+						</li>
+					</ul>
+				</li>
+				<li>Ritesh Hiremath - Alumni Adviser<ul>
+						<li>
+							
+						</li>
+					</ul>
+				</li>
+				<li>Dayasagar Radhakrishnan - Graphic designer<ul>
+						<li>
+							
+						</li>
+					</ul>
+				</li>
+				<li>Divya Kumarasubramanian - Treasurer<ul>
+						<li>
+							
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</div>
+		<div >
+			<ul>
+			</ul>
+		</div>
+		<div class="col-sm-4">
+			<h3>Column 2</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+			<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+		</div>
+		<div class="col-sm-4">
+			<h3>Column 3</h3>				
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>
+			<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
